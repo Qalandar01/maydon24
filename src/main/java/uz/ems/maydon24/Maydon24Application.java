95080c89aa74077b0e611bbf152dad25fc110086
Qalandar01 <qalandar2201@gmail.com>
2025-10-19 14:50:32 +0500

    Initial commit

---
+++ src/main/java/uz/ems/maydon24/Maydon24Application.java
@@ -0,0 +1,13 @@
+package uz.ems.maydon24;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Maydon24Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Maydon24Application.class, args);
+    }
+
+}
---
+++ src/test/java/uz/ems/maydon24/Maydon24ApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.ems.maydon24;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Maydon24ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
