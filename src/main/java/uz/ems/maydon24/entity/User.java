74376bb85d2a19f5c7d5e7ff6f1f5ba5e70a55dc
Qalandar01 <qalandar2201@gmail.com>
2025-10-19 16:08:52 +0500

    user added

---
+++ src/main/java/uz/ems/maydon24/entity/User.java
@@ -0,0 +1,62 @@
+package uz.ems.maydon24.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import lombok.*;
+import org.hibernate.annotations.SQLRestriction;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import uz.ems.maydon24.base.BaseEntity;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+@Table(name = "users")
+@SQLRestriction("visibility=true")
+public class User extends BaseEntity implements UserDetails {
+
+    private String tgUsername;
+
+    private String fullName;
+
+    private String phone;
+
+    private Integer messageId;
+
+    private Integer verifyCode;
+
+    private boolean visibility = true;
+
+    @ManyToMany
+    private Set<Role> roles;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.roles;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return this.visibility;
+    }
+
+    @Override
+    public String getPassword() {
+        return "[PROTECTED]";
+    }
+
+    @Override
+    public String getUsername(){
+        return this.phone;
+    }
+}
