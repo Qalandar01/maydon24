aa6f3db8e15e38d4ffe539ba7c8714610c342718
Suhrob <suhrob_kalandarov@outlook.com>
2025-10-20 18:25:03 +0500

    Add `UserService` interface with `getOrCreateUser` method

---
+++ src/main/java/uz/ems/maydon24/config/botauth/service/face/UserService.java
@@ -0,0 +1,11 @@
+package uz.ems.maydon24.config.botauth.service.face;
+
+import org.springframework.stereotype.Service;
+import uz.ems.maydon24.models.entity.User;
+
+@Service
+public interface UserService {
+
+    User getOrCreateUser(com.pengrad.telegrambot.model.User tgUser);
+
+}
