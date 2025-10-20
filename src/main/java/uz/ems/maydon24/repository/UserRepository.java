b011c17d4ec2ff1b59716b8231908e0a0338f073
Suhrob <suhrob_kalandarov@outlook.com>
2025-10-20 18:24:26 +0500

    Add `findByTelegramId` method to `UserRepository`

--- src/main/java/uz/ems/maydon24/repository/UserRepository.java
+++ src/main/java/uz/ems/maydon24/repository/UserRepository.java
@@ -1,6 +1,6 @@
 package uz.ems.maydon24.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.*;
 import org.springframework.stereotype.Repository;
 import uz.ems.maydon24.models.entity.User;
 
@@ -8,5 +8,8 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
     Optional<User> findByPhoneNumber(String phoneNumber);
+
+    Optional<User> findByTelegramId(Long telegramId);
 }
