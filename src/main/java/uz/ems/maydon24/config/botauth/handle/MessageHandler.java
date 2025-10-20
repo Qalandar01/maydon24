927b899ba9503d7371704ab01ebdc244dafc11e2
Suhrob <suhrob_kalandarov@outlook.com>
2025-10-20 18:35:29 +0500

    Integrate `MessageService` into `MessageHandler` to handle `/start` command and send start messages via Telegram Bot

--- src/main/java/uz/ems/maydon24/config/botauth/handle/MessageHandler.java
+++ src/main/java/uz/ems/maydon24/config/botauth/handle/MessageHandler.java
@@ -4,6 +4,7 @@ import com.pengrad.telegrambot.model.Message;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import uz.ems.maydon24.config.botauth.service.face.MessageService;
 import uz.ems.maydon24.config.botauth.service.face.UserService;
 import uz.ems.maydon24.models.entity.User;
 
@@ -15,11 +16,16 @@ import java.util.function.Consumer;
 public class MessageHandler implements Consumer<Message> {
 
     private final UserService userService;
+    private final MessageService messageService;
 
     @Override
     public void accept(Message message) {
         String text = message.text();
         User user = userService.getOrCreateUser(message.from());
 
+        if (text.equals("/start")) {
+            messageService.sendStartMsg(user.getTelegramId(), user.getFullName());
+
+        } else log.warn("Unknown message {}", text);
     }
 }
