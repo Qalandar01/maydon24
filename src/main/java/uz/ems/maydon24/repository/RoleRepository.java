abe1f0c2666def17a69ba42fb712bdc416493e8f
Suhrob <suhrob_kalandarov@outlook.com>
2025-10-20 18:22:49 +0500

    Update `RoleRepository` with methods to find roles by name and multiple names

--- src/main/java/uz/ems/maydon24/repository/RoleRepository.java
+++ src/main/java/uz/ems/maydon24/repository/RoleRepository.java
@@ -3,7 +3,15 @@ package uz.ems.maydon24.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import uz.ems.maydon24.models.entity.Role;
+import uz.ems.maydon24.models.enums.RoleName;
+
+import java.util.Optional;
+import java.util.Set;
 
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
+
+    Optional<Role> findByName(RoleName name);
+
+    Set<Role> findAllByNameIn(Set<RoleName> names);
 }
